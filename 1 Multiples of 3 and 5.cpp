4b7de0e5a15338743a6733f760683af8f51a193a
Naman Jain <58143971+namanworld@users.noreply.github.com>
2020-06-11 23:37:14 +0530

    Create 1 Multiples of 3 and 5.cpp

---
+++ 1 Multiples of 3 and 5.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main()
+{
+    int n;
+    cin>>n;
+    int ans = 0;
+    for(int i=1; i<n; i++){
+        if(i%3==0 || i%5==0) ans+=i;
+    }
+    cout<<ans;
+    return 0;
+}
